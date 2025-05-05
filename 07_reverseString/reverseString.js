2c1840096fea6d320139924ac3e6c83645530968
ViniciusAD <vinicius.ataide.delgado@ccc.ufcg.edu.br>
2025-05-05 17:52:58 -0300

    Add exercise 7 solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    const intermediateList = input.split("");
 
+    let returnedValue = "";
+    for (let i = intermediateList.length - 1; i >= 0; i--) {
+        returnedValue += intermediateList[i];
+    }
+
+    return returnedValue;
 };
 
 // Do not edit below this line
