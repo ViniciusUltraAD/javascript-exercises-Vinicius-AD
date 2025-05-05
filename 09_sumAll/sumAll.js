937545cddac36caad8ea25213a2edca24d20a000
ViniciusAD <vinicius.ataide.delgado@ccc.ufcg.edu.br>
2025-05-05 17:53:39 -0300

    Add exercise 9 solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return "ERROR";
+    }
+    
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
 
+    if (firstNumber > secondNumber) {
+        const temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+
+    sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
