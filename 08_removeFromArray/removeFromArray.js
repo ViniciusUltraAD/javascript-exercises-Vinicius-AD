8ba361a8d40e9ebc736d92a1e78e11f47085ea11
ViniciusAD <vinicius.ataide.delgado@ccc.ufcg.edu.br>
2025-05-05 17:53:19 -0300

    Add exercise 8 solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elemsToRemove) {
+    for (let i = inputArray.length - 1; i >= 0; i--) {
+        if (elemsToRemove.includes(inputArray[i])) {
+            inputArray.splice(i, 1);
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
