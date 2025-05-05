0672361288da2a7215319e94f420a8c1a3656ca8
ViniciusAD <vinicius.ataide.delgado@ccc.ufcg.edu.br>
2025-05-05 17:52:40 -0300

    Add exercise 6 solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(value, reps) {
+    if (reps < 0) {
+        return "ERROR";
+    } 
 
+    let returnedValue = "";
+
+    for (let i = 0; i < reps; i++) {
+        returnedValue += value;
+    }
+
+    return returnedValue;
 };
 
 // Do not edit below this line
